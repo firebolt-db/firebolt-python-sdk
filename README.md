6f85d076978ee00ee0a256d333363aa81b4bbdaf
Eric Gustavson <eg@firebolt.io>
2021-08-04 16:03:31 -0700

    scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # firebolt-sdk
+
+### Contributing
+
+1. clone this repo into a virtual environment
+1. `pip install -e ".[dev]"`
+1. `pre-commit install`
+1. if using pycharm, set the test runner as `pytest` and mark `src` as a sources root
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="firebolt",
+    description="Python SDK for Firebolt",
+    author="Eric Gustavson",
+    author_email="eg@firebolt.io",
+    url="https://github.com/firebolt-analytics/firebolt-sdk",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    python_requires=">=3.7",
+    install_requires=["requests", "requests-toolbelt"],
+    extras_require={
+        "dev": [
+            "pre-commit",
+            "pytest",
+            "pytest-mock",
+        ]
+    },
+)
