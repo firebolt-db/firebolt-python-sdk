a9f13a5667c14f37d68c57a57338b079c0bda48c
Stepan Burlakov <stepansergeevitch@gmail.com>
2021-09-15 17:51:22 +0300

    refactor tests

--- tests/client/test_auth.py
+++ tests/client/test_auth.py
@@ -1,82 +1,77 @@
-import json
+import typing
 
 import httpx
 import pytest
-from pytest_httpx import HTTPXMock, to_response
+from pytest_httpx import HTTPXMock
 from pytest_mock import MockerFixture
 
 from firebolt.client import AuthenticationError, FireboltAuth
 
-TEST_TOKEN: str = "test_token"
-TEST_TOKEN2: str = "test_token2"
-TEST_USERNAME: str = "username"
-TEST_PASSWORD: str = "password"
 
-
-def test_auth_basic(httpx_mock: HTTPXMock, mocker: MockerFixture):
+def test_auth_basic(
+    httpx_mock: HTTPXMock,
+    mocker: MockerFixture,
+    check_credentials_callback: typing.Callable,
+    test_username,
+    test_password,
+    test_token,
+):
     "FireboltAuth can retrieve token and expiration values"
 
-    def check_credentials(
-        request: httpx.Request = None, **kwargs
-    ) -> httpx.Response:
-        assert request, "empty request"
-        body = json.loads(request.read())
-        assert "username" in body, "Missing username"
-        assert body["username"] == TEST_USERNAME, "Invalid username"
-        assert "password" in body, "Missing password"
-        assert body["password"] == TEST_PASSWORD, "Invalid password"
-
-        return to_response(
-            status_code=httpx.codes.OK,
-            json={"expiry": 2 ** 32, "access_token": TEST_TOKEN},
-        )
-
-    httpx_mock.add_callback(check_credentials)
+    httpx_mock.add_callback(check_credentials_callback)
 
     mocker.patch("time.time", return_value=0)
-    auth = FireboltAuth(TEST_USERNAME, TEST_PASSWORD)
-    assert auth.token == TEST_TOKEN, "invalid access token"
+    auth = FireboltAuth(test_username, test_password)
+    assert auth.token == test_token, "invalid access token"
     assert auth._expires == 2 ** 32, "invalid expiration value"
 
 
-def test_auth_refresh_on_expiration(httpx_mock: HTTPXMock):
+def test_auth_refresh_on_expiration(
+    httpx_mock: HTTPXMock,
+    test_token: str,
+    test_token2: str,
+):
     "FireboltAuth refreshes token on expiration"
 
     # To get token for the first time
     httpx_mock.add_response(
         status_code=httpx.codes.OK,
-        json={"expiry": 0, "access_token": TEST_TOKEN},
+        json={"expiry": 0, "access_token": test_token},
     )
 
     # To refresh token
     httpx_mock.add_response(
         status_code=httpx.codes.OK,
-        json={"expiry": 0, "access_token": TEST_TOKEN2},
+        json={"expiry": 0, "access_token": test_token2},
     )
 
     auth = FireboltAuth("user", "password")
-    assert auth.token == TEST_TOKEN, "invalid access token"
-    assert auth.token == TEST_TOKEN2, "expired access token was not updated"
+    assert auth.token == test_token, "invalid access token"
+    assert auth.token == test_token2, "expired access token was not updated"
 
 
-def test_auth_uses_same_token_if_valid(httpx_mock: HTTPXMock):
+def test_auth_uses_same_token_if_valid(
+    httpx_mock: HTTPXMock,
+    test_token: str,
+    test_token2: str,
+):
     "FireboltAuth refreshes token on expiration"
 
     # To get token for the first time
     httpx_mock.add_response(
         status_code=httpx.codes.OK,
-        json={"expiry": 2 ** 32, "access_token": TEST_TOKEN},
+        json={"expiry": 2 ** 32, "access_token": test_token},
     )
 
     # To refresh token
     httpx_mock.add_response(
         status_code=httpx.codes.OK,
-        json={"expiry": 2 ** 32, "access_token": TEST_TOKEN2},
+        json={"expiry": 2 ** 32, "access_token": test_token2},
     )
 
     auth = FireboltAuth("user", "password")
-    assert auth.token == TEST_TOKEN, "invalid access token"
-    assert auth.token == TEST_TOKEN, "shoud not update token until it expires"
+    assert auth.token == test_token, "invalid access token"
+    assert auth.token == test_token, "shoud not update token until it expires"
     httpx_mock.reset(False)
 
 
@@ -124,16 +119,19 @@ def test_auth_error_handling(httpx_mock: HTTPXMock):
     httpx_mock.reset(True)
 
 
-def test_auth_adds_header(httpx_mock):
+def test_auth_adds_header(
+    httpx_mock: HTTPXMock,
+    test_token: str,
+):
     "FireboltAuth adds required authentication headers to httpx.Request"
     httpx_mock.add_response(
         status_code=httpx.codes.OK,
-        json={"expiry": 0, "access_token": TEST_TOKEN},
+        json={"expiry": 0, "access_token": test_token},
     )
 
     auth = FireboltAuth("user", "password")
     request = next(auth.auth_flow(httpx.Request("get", "")))
     assert "authorization" in request.headers, "missing authorization header"
     assert (
-        request.headers["authorization"] == f"Bearer {TEST_TOKEN}"
+        request.headers["authorization"] == f"Bearer {test_token}"
     ), "missing authorization header"
--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -1,27 +1,28 @@
-import json
+import typing
 
 import httpx
 import pytest
-from pytest_httpx import HTTPXMock, to_response
+from pytest_httpx import HTTPXMock
 
 from firebolt.client import DEFAULT_API_URL, FireboltAuth, FireboltClient
 
-TEST_TOKEN: str = "test_token"
-TEST_USERNAME: str = "username"
-TEST_PASSWORD: str = "password"
 
-
-def test_client_retry(httpx_mock: HTTPXMock):
+def test_client_retry(
+    httpx_mock: HTTPXMock,
+    test_username: str,
+    test_password: str,
+    test_token: str,
+):
     """
     Firebolt client retries with new auth token
     if first attempt fails with Unauthorized error
     """
-    client = FireboltClient(auth=(TEST_USERNAME, TEST_PASSWORD))
+    client = FireboltClient(auth=(test_username, test_password))
 
     # auth get token
     httpx_mock.add_response(
         status_code=httpx.codes.OK,
-        json={"expiry": 2 ** 30, "access_token": TEST_TOKEN},
+        json={"expiry": 2 ** 30, "access_token": test_token},
     )
 
     # client request failed authorization
@@ -32,7 +33,7 @@ def test_client_retry(httpx_mock: HTTPXMock):
     # auth get another token
     httpx_mock.add_response(
         status_code=httpx.codes.OK,
-        json={"expiry": 2 ** 30, "access_token": TEST_TOKEN},
+        json={"expiry": 2 ** 30, "access_token": test_token},
     )
 
     # client request success this time
@@ -45,7 +46,13 @@ def test_client_retry(httpx_mock: HTTPXMock):
     ), "request failed with firebolt client"
 
 
-def test_client_different_auths(httpx_mock: HTTPXMock):
+def test_client_different_auths(
+    httpx_mock: HTTPXMock,
+    check_credentials_callback: typing.Callable,
+    check_token_callback: typing.Callable,
+    test_username: str,
+    test_password: str,
+):
     """
     Firebolt propperly handles such auth types:
     - tuple(username, password)
@@ -54,42 +61,15 @@ def test_client_different_auths(httpx_mock: HTTPXMock):
     All other types should raise TypeError
     """
 
-    def check_credentials(
-        request: httpx.Request = None, **kwargs
-    ) -> httpx.Response:
-        assert request, "empty request"
-        body = json.loads(request.read())
-        assert "username" in body, "Missing username"
-        assert body["username"] == TEST_USERNAME, "Invalid username"
-        assert "password" in body, "Missing password"
-        assert body["password"] == TEST_PASSWORD, "Invalid password"
-
-        return to_response(
-            status_code=httpx.codes.OK,
-            json={"expiry": 2 ** 32, "access_token": TEST_TOKEN},
-        )
-
-    def check_token(request: httpx.Request = None, **kwargs) -> httpx.Response:
-        prefix = "Bearer "
-        assert request, "empty request"
-        assert (
-            "authorization" in request.headers
-        ), "missing authorization header"
-        auth = request.headers["authorization"]
-        assert auth.startswith(prefix), "invalid authorization header format"
-        token = auth[len(prefix) :]
-        assert token == TEST_TOKEN, "invalid authorization token"
-
-        return to_response(status_code=httpx.codes.OK)
-
     httpx_mock.add_callback(
-        check_credentials, url=f"https://{DEFAULT_API_URL}/auth/v1/login"
+        check_credentials_callback,
+        url=f"https://{DEFAULT_API_URL}/auth/v1/login",
     )
 
-    httpx_mock.add_callback(check_token, url="https://url")
+    httpx_mock.add_callback(check_token_callback, url="https://url")
 
-    FireboltClient(auth=(TEST_USERNAME, TEST_PASSWORD)).get("https://url")
-    FireboltClient(auth=FireboltAuth(TEST_USERNAME, TEST_PASSWORD)).get(
+    FireboltClient(auth=(test_username, test_password)).get("https://url")
+    FireboltClient(auth=FireboltAuth(test_username, test_password)).get(
         "https://url"
     )
 
