0e6f2d7a14e5240f8d46b8ec7cf279050eb6c702
Stepan Burlakov <stepansergeevitch@gmail.com>
2021-09-16 15:34:43 +0300

    move firebolt_client tests to pytest_httpx

--- tests/test_firebolt_client.py
+++ tests/test_firebolt_client.py
@@ -1,18 +1,44 @@
+import httpx
+from pytest_httpx import HTTPXMock
+
+from firebolt.common import Settings
 from firebolt.firebolt_client import FireboltClient
 
 
-def test_settings(mocked_api, settings):
+def test_settings(settings: Settings, httpx_mock: HTTPXMock):
+    httpx_mock.add_response(
+        url=f"https://{settings.server}/auth/v1/login",
+        status_code=httpx.codes.OK,
+        json={"access_token": ""},
+    )
     with FireboltClient(settings=settings) as fc:
         assert fc.settings == settings
 
 
-def test_auth(mocked_api, settings, access_token):
+def test_auth(
+    httpx_mock: HTTPXMock,
+    settings: Settings,
+    access_token: str,
+):
+    httpx_mock.add_response(
+        url=f"https://{settings.server}/auth/v1/login",
+        status_code=httpx.codes.OK,
+        json={"access_token": access_token},
+    )
     with FireboltClient(settings=settings) as fc:
-        assert mocked_api["auth"].called
         assert fc.access_token == access_token
 
 
-def test_account_id(mocked_api, settings, account_id):
+def test_account_id(httpx_mock: HTTPXMock, settings: Settings, account_id: str):
+    httpx_mock.add_response(
+        url=f"https://{settings.server}/auth/v1/login",
+        status_code=httpx.codes.OK,
+        json={"access_token": ""},
+    )
+    httpx_mock.add_response(
+        url=f"https://{settings.server}/iam/v2/account",
+        status_code=httpx.codes.OK,
+        json={"account": {"id": account_id}},
+    )
     with FireboltClient(settings=settings) as fc:
         assert fc.account_id == account_id
-        assert mocked_api["account_id"].called
