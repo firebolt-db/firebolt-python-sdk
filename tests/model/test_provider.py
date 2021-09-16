05d99ae5935827c5343ee05018f260ce62f45f28
Stepan Burlakov <stepansergeevitch@gmail.com>
2021-09-16 15:54:29 +0300

    move model tests to pytest_httpx

--- tests/model/test_provider.py
+++ tests/model/test_provider.py
@@ -1,8 +1,25 @@
+from typing import List
+
+import httpx
+from pytest_httpx import HTTPXMock
+
+from firebolt.common import Settings
 from firebolt.firebolt_client import FireboltClient
-from firebolt.model.provider import providers
+from firebolt.model.provider import Provider, providers
 
 
-def test_provider(mocked_api, settings, mock_providers):
+def test_provider(
+    httpx_mock: HTTPXMock, settings: Settings, mock_providers: List[Provider]
+):
+    httpx_mock.add_response(
+        url=f"https://{settings.server}/auth/v1/login",
+        status_code=httpx.codes.OK,
+        json={"access_token": ""},
+    )
+    httpx_mock.add_response(
+        url=f"https://{settings.server}/compute/v1/providers?page.first=5000",
+        status_code=httpx.codes.OK,
+        json={"edges": [{"node": it.dict()} for it in mock_providers]},
+    )
     with FireboltClient(settings=settings):
         assert providers.providers == mock_providers
-        assert mocked_api["providers"].called
--- tests/model/test_region.py
+++ tests/model/test_region.py
@@ -1,8 +1,23 @@
+from typing import List
+
+import httpx
+from pytest_httpx import HTTPXMock
+
+from firebolt.common import Settings
 from firebolt.firebolt_client import FireboltClient
-from firebolt.model.region import regions
+from firebolt.model.region import Region, regions
 
 
-def test_region(mocked_api, settings, mock_regions):
+def test_region(httpx_mock: HTTPXMock, settings: Settings, mock_regions: List[Region]):
+    httpx_mock.add_response(
+        url=f"https://{settings.server}/auth/v1/login",
+        status_code=httpx.codes.OK,
+        json={"access_token": ""},
+    )
+    httpx_mock.add_response(
+        url=f"https://{settings.server}/compute/v1/regions?page.first=5000",
+        status_code=httpx.codes.OK,
+        json={"edges": [{"node": it.dict()} for it in mock_regions]},
+    )
     with FireboltClient(settings=settings):
         assert regions.regions == mock_regions
-        assert mocked_api["regions"].called
