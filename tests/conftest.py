cee297dfb467b9d97fbc103d1bba2c7498a07121
Stepan Burlakov <stepansergeevitch@gmail.com>
2021-09-16 16:00:29 +0300

    respx cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,7 @@
 import pytest
-import respx
-from httpx import Response
 from pydantic import SecretStr
 
 from firebolt.common.settings import Settings
-from firebolt.model import FireboltBaseModel
 from firebolt.model.instance_type import InstanceType, InstanceTypeKey
 from firebolt.model.provider import Provider
 from firebolt.model.region import Region, RegionKey
@@ -92,44 +89,6 @@ def instance_type_2(provider, region_2) -> InstanceType:
 @pytest.fixture
 def mock_instance_types(instance_type_1, instance_type_2) -> list[InstanceType]:
     return [instance_type_1, instance_type_2]
-
-
-def paginated(items: list[FireboltBaseModel]) -> dict:
-    """
-    Wrap a list in "edge" and "nodes" to mimic how
-    the Firebolt API returns paginated reponses.
-    """
-    return {"edges": [{"node": i.dict()} for i in items]}
-
-
-@pytest.fixture
-def mocked_api(
-    server, access_token, account_id, mock_providers, mock_regions, mock_instance_types
-):
-    with respx.mock(
-        base_url=f"https://{server}", assert_all_called=False
-    ) as respx_mock:
-        auth_route = respx_mock.post("/auth/v1/login", name="auth")
-        auth_route.return_value = Response(200, json={"access_token": access_token})
-
-        account_id_route = respx_mock.get("/iam/v2/account", name="account_id")
-        account_id_route.return_value = Response(
-            200, json={"account": {"id": account_id}}
-        )
-
-        providers_route = respx_mock.get("/compute/v1/providers", name="providers")
-        providers_route.return_value = Response(200, json=paginated(mock_providers))
-
-        regions_route = respx_mock.get("/compute/v1/regions", name="regions")
-        regions_route.return_value = Response(200, json=paginated(mock_regions))
-
-        instance_types_route = respx_mock.get(
-            "/compute/v1/instanceTypes", name="instance_types"
-        )
-        instance_types_route.return_value = Response(
-            200, json=paginated(mock_instance_types)
-        )
-        yield respx_mock
 
 
 @pytest.fixture
