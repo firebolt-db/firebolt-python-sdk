edc43be2ad0c95035636124ba77b0864fed08d87
Burlakov Stepan <stepansergeevitch@gmail.com>
2021-09-17 15:37:03 +0300

    Fir 7912 migrate sdk tests from respx to (#5)
    * move firebolt_client tests to pytest_httpx
    
    * move model tests to pytest_httpx
    
    * respx cleanup

--- src/firebolt/common/__init__.py
+++ src/firebolt/common/__init__.py
@@ -1,3 +1,14 @@
+from .exception import (
+    AlreadyBoundError,
+    DatabaseRequiredError,
+    EndpointRequiredError,
+    FireboltClientRequiredError,
+    FireboltEngineError,
+    FireboltError,
+)
+from .settings import Settings
+
+
 def prune_dict(d: dict) -> dict:
     """Prune items from dictionaries where value is None"""
     return {k: v for k, v in d.items() if v is not None}
--- tests/model/test_instance_type.py
+++ tests/model/test_instance_type.py
@@ -1,8 +1,25 @@
+from typing import List
+
+import httpx
+from pytest_httpx import HTTPXMock
+
+from firebolt.common import Settings
 from firebolt.firebolt_client import FireboltClient
-from firebolt.model.instance_type import instance_types
+from firebolt.model.instance_type import InstanceType, instance_types
 
 
-def test_instance_type(mocked_api, settings, mock_instance_types):
+def test_instance_type(
+    httpx_mock: HTTPXMock, settings: Settings, mock_instance_types: List[InstanceType]
+):
+    httpx_mock.add_response(
+        url=f"https://{settings.server}/auth/v1/login",
+        status_code=httpx.codes.OK,
+        json={"access_token": ""},
+    )
+    httpx_mock.add_response(
+        url=f"https://{settings.server}/compute/v1/instanceTypes?page.first=5000",
+        status_code=httpx.codes.OK,
+        json={"edges": [{"node": it.dict()} for it in mock_instance_types]},
+    )
     with FireboltClient(settings=settings):
         assert instance_types.instance_types == mock_instance_types
-        assert mocked_api["instance_types"].called
