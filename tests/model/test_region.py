cb779cdd570a532bc59dc17b8103d7bc2e40961e
Eric Gustavson <88102198+eg-firebolt@users.noreply.github.com>
2021-09-21 08:38:46 -0700

    refactor: test cleanup (#8)
    * refactor: test cleanup

--- tests/model/test_region.py
+++ tests/model/test_region.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Callable, List
 
 import httpx
 from pytest_httpx import HTTPXMock
@@ -6,18 +6,20 @@ from pytest_httpx import HTTPXMock
 from firebolt.common import Settings
 from firebolt.firebolt_client import FireboltClient
 from firebolt.model.region import Region, regions
+from tests.util import list_to_paginated_response
 
 
-def test_region(httpx_mock: HTTPXMock, settings: Settings, mock_regions: List[Region]):
-    httpx_mock.add_response(
-        url=f"https://{settings.server}/auth/v1/login",
-        status_code=httpx.codes.OK,
-        json={"access_token": ""},
-    )
+def test_region(
+    httpx_mock: HTTPXMock,
+    httpx_mock_auth_callback: Callable,
+    settings: Settings,
+    mock_regions: List[Region],
+):
+    httpx_mock.add_callback(httpx_mock_auth_callback)
     httpx_mock.add_response(
         url=f"https://{settings.server}/compute/v1/regions?page.first=5000",
         status_code=httpx.codes.OK,
-        json={"edges": [{"node": it.dict()} for it in mock_regions]},
+        json=list_to_paginated_response(mock_regions),
     )
     with FireboltClient(settings=settings):
         assert regions.regions == mock_regions
