a531306a05aba0c9563fc2b880e2fd86dd89e0ab
Stepan Burlakov <stepansergeevitch@gmail.com>
2021-09-28 18:38:55 +0300

    add module-level constants

--- src/firebolt/db/__init__.py
+++ src/firebolt/db/__init__.py
@@ -1,2 +1,6 @@
 from firebolt.db.connection import Connection
 from firebolt.db.cursor import Cursor
+
+apilevel = "2.0"
+threadsafety = 3  # threads may share the module, connections and cursors
+paramstyle = "qmark"
