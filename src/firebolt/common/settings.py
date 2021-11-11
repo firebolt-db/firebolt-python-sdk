b03c44cea706121e9003b7609b66838c1e8a65d3
Eric Ford <ericford@mac.com>
2021-11-10 22:22:39 -0500

    Attempting to fix issues with github build.

--- src/firebolt/common/settings.py
+++ src/firebolt/common/settings.py
@@ -3,11 +3,11 @@ from typing import Optional
 
 
 class Settings(BaseSettings):
+    account_name: Optional[str] = Field(..., env="FIREBOLT_ACCOUNT")
     server: str = Field(..., env="FIREBOLT_SERVER")
     user: str = Field(..., env="FIREBOLT_USER")
     password: SecretStr = Field(..., env="FIREBOLT_PASSWORD")
     default_region: str = Field(..., env="FIREBOLT_DEFAULT_REGION")
-    account_name: Optional[str] = Field(..., env="FIREBOLT_ACCOUNT")
 
     class Config:
         env_file = ".env"
