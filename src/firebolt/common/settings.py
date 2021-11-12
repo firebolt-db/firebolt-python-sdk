8f085d4732881c7720f42146e5cc37e1cc52f613
Eric Ford <ericford@mac.com>
2021-11-12 15:43:08 -0500

    Made account_name Field in settings optional the _correct_ way.

--- src/firebolt/common/settings.py
+++ src/firebolt/common/settings.py
@@ -1,9 +1,8 @@
 from pydantic import BaseSettings, Field, SecretStr
-from typing import Optional
 
 
 class Settings(BaseSettings):
-    account_name: Optional[str] = Field(..., env="FIREBOLT_ACCOUNT")
+    account_name: str = Field(None, env="FIREBOLT_ACCOUNT")
     server: str = Field(..., env="FIREBOLT_SERVER")
     user: str = Field(..., env="FIREBOLT_USER")
     password: SecretStr = Field(..., env="FIREBOLT_PASSWORD")
