0ea47b46d69a76d90c3bef54a912d948de9476e7
Petro <ptiurin@gmail.com>
2021-11-23 14:26:55 +0000

    Fixing sync tests

--- tests/integration/dbapi/sync/test_errors.py
+++ tests/integration/dbapi/sync/test_errors.py
@@ -3,9 +3,10 @@ from pytest import raises
 
 from firebolt.common.exception import (
     AuthenticationError,
+    EngineNotRunningError,
+    FireboltDatabaseError,
     FireboltEngineError,
     OperationalError,
-    ProgrammingError,
 )
 from firebolt.db import Connection, connect
 
@@ -63,6 +64,25 @@ def test_engine_name_not_exists(
         connection.cursor().execute("show tables")
 
 
+def test_engine_stopped(
+    stopped_engine_url: str,
+    database_name: str,
+    username: str,
+    password: str,
+    api_endpoint: str,
+) -> None:
+    """Connection properly reacts to invalid engine name error"""
+    with raises(EngineNotRunningError):
+        connection = connect(
+            engine_url=stopped_engine_url,
+            database=database_name,
+            username=username,
+            password=password,
+            api_endpoint=api_endpoint,
+        )
+        connection.cursor().execute("show tables")
+
+
 def test_database_not_exists(
     engine_url: str, database_name: str, username: str, password: str, api_endpoint: str
 ) -> None:
@@ -75,11 +95,11 @@ def test_database_not_exists(
         password=password,
         api_endpoint=api_endpoint,
     )
-    with raises(ProgrammingError) as exc_info:
+    with raises(FireboltDatabaseError) as exc_info:
         connection.cursor().execute("show tables")
 
     assert (
-        str(exc_info.value) == f"Invalid database '{new_db_name}'"
+        str(exc_info.value) == f"Database {new_db_name} does not exist"
     ), "Invalid database name error message"
 
 
