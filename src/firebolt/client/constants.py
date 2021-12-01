62666a4ebd0141ef4fafc0e0a1f0e25a14091b85
Burlakov Stepan <stepansergeevitch@gmail.com>
2021-12-01 14:19:40 +0200

    fix: increase timeout for resource manager operations (#64)
    * increase timeout for resource manager operations
    
    * remove unsed constant

--- src/firebolt/client/constants.py
+++ src/firebolt/client/constants.py
@@ -1,10 +1,9 @@
 from json import JSONDecodeError
-from typing import Optional, Tuple, Type
+from typing import Tuple, Type
 
 from httpx import CookieConflict, HTTPError, InvalidURL, StreamError
 
 DEFAULT_API_URL: str = "api.app.firebolt.io"
-API_REQUEST_TIMEOUT_SECONDS: Optional[int] = 60
 _REQUEST_ERRORS: Tuple[Type, ...] = (
     HTTPError,
     InvalidURL,
