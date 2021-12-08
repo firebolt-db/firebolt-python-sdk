f715812210d3ce1e703f3156995eaa70043e1b61
Burlakov Stepan <stepansergeevitch@gmail.com>
2021-12-08 10:42:26 +0200

    fix: async_to_sync tests (#72)
    * add async_to_sync test
    
    * fix async_to_sync test
    
    * extend async_to_sync tests

---
+++ tests/unit/common/test_util.py
@@ -0,0 +1,52 @@
+from asyncio import run
+from threading import Thread
+
+from pytest import raises
+
+from firebolt.common.util import async_to_sync
+
+
+def test_async_to_sync_happy_path():
+    """async_to_sync properly converts coroutine to sync function"""
+
+    class JobMarker(Exception):
+        pass
+
+    async def task():
+        raise JobMarker()
+
+    for i in range(3):
+        with raises(JobMarker):
+            async_to_sync(task)()
+
+
+def test_async_to_sync_thread():
+    """async_to_sync properly works in threads"""
+
+    marks = [False] * 3
+
+    async def task(id: int):
+        marks[id] = True
+
+    ts = [Thread(target=async_to_sync(task), args=[i]) for i in range(3)]
+    [t.start() for t in ts]
+    [t.join() for t in ts]
+    assert all(marks)
+
+
+def test_async_to_sync_after_run():
+    """async_to_sync properly runs after asyncio.run"""
+
+    class JobMarker(Exception):
+        pass
+
+    async def task():
+        raise JobMarker()
+
+    with raises(JobMarker):
+        run(task())
+
+    # Here local event loop is closed by run
+
+    with raises(JobMarker):
+        async_to_sync(task)()
