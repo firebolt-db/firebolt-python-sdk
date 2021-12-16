1c18df1e0aa58026e869cc3857cebd9becfe0c7a
marknoack <mark.noack@firebolt.io>
2021-12-16 10:56:51 -0800

    Test for Google docstring #2

--- src/firebolt/async_db/connection.py
+++ src/firebolt/async_db/connection.py
@@ -67,18 +67,15 @@ def async_connect_factory(connection_class: Type) -> Callable:
         """
         Connect to Firebolt database.
 
-        Args: 
+        :param database: name of the database to connect
+        :param username: user name to use for authentication
+        :param password: password to use for authentication
+        :param engine_name: The name of the engine to connect to
+        :type engine_name: optional
+        :param engine_url: The engine endpoint to use
+        :type engine_url: optional
+        .. note:: either `engine_name` or `engine_url` should be provided, but not both
 
-            :param database: name of the database to connect
-            :param username: user name to use for authentication
-            :param password: password to use for authentication
-            :param engine_name: The name of the engine to connect to
-            :type engine_name: optional
-            :param engine_url: The engine endpoint to use
-            :type engine_url: optional
-        
-        Note: 
-            either `engine_name` or `engine_url` should be provided, but not both
         """
 
         if engine_name and engine_url:
