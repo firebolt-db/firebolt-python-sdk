firebolt-db/firebolt-python-sdk

commit 0ee2d4ece9b08a63ac36e5562d3cd42e732aceca

Add @async_cached_property back to client.py.

src/firebolt/client/client.py
@@ -1,5 +1,6 @@
 from typing import Any, Optional
 
+from async_property import async_cached_property  # type: ignore
 from httpx import AsyncClient as HttpxAsyncClient
 from httpx import Client as HttpxClient
 from httpx import _types
@@ -83,6 +84,7 @@ class AsyncClient(FireboltClientMixin, HttpxAsyncClient):
     + (HttpxAsyncClient.__doc__ or "")
     """
 
+    @async_cached_property
     async def account_id(self) -> str:
         if self.account_name is not None:
             response = await self.get(

commit a5e099da3dd1de540a2d345a38ec58861d393440

feat: Add account_name to DB-API (#86)
* Added account_name to client and settings and connection, where client is called. Removed logic to retrieve account_id from manager; replaced with logic in client.

* Added account_name where required in asynch connection.py.

* Added account_name to unit/db/text_connection. Added clarifying notes to docstrings in asynch connections.py and connections.py.

* Added account_name to Settings in unit/async_db/conftest.py.

* Added account_name to readme_management.md. Added url and url_callback for account_id.

* Edited tests/unit/async_db/test_connection.py to correct account_id urls.

* Added account_name to an object instantionation in async_db/connection.py. Fixed errors in account_id_callback in unit/conftest.py.

* Added AccountNameError to deal with instances where an incorrect account name is used.

* Added invalid account name exception and error test.

* Added test_invalid_account() to dbapi/sync/test_errors.py.

* Added a bunch of periods to assertion error messages.

* Changed account_name from Optional in FireboltClientMixin to satisfy mypy. Also, a couple of periods added in exception messages.

* Try two to change account_name from Optional in FireboltClientMixin to satisfy mypy.

* Try three: changed account_name to Optional in async_connect_factory and FireboltClientMixin  to satisfy mypy.

* Passed mypy by setting account_name to Optional[str] in _resolve_engine_url.

* Removed and replaced ENGINE_BY_NAME_URL with ACCOUNT_ENGINE_BY_NAME_URL everywhere. Made account_name type to Optional[str]=None in _resolve_engine_url. Added account_name to conftests. Minor text corrections.

* Couldn't help myself: added a bunch of periods to test_queries_async.py.

* Removed unused _get_account_id() from service/manager.py.

* Removed all edits to comments and documentation. Will be added in separate branch.

* Removed an extra call to retrieve account_id in connection.py.

* Changed AccountError to AccountNotFoundError and used httpx.codes for no response error on missing account_name.

* Mistyped httpx in import in client.py.

* Removed accidental change from asyncio to trio in common/utils.py.

* Removed account_name completely from Connection class.

* Small cleanups to deal with a failed rebase.

* Add @async_cached_property back to client.py.

* Fixed await error caused by last commit.

* account_id_callback is unused in test_cursor_initialized in asynch_db/test_connection. Commented it out for now.

* Fixed unit test errors caused by 0ee2d4ece9b08a63ac36e5562d3cd42e732aceca.

setup.cfg
@@ -28,7 +28,6 @@ install_requires =
     httpx[http2]==0.19.0
     pydantic[dotenv]==1.8.2
     readerwriterlock==1.0.9
-    syncer==1.3.0
 python_requires = >=3.7
 include_package_data = True
 package_dir =

src/firebolt/async_db/connection.py
@@ -13,28 +13,33 @@ from firebolt.common.exception import (
     FireboltEngineError,
     InterfaceError,
 )
-from firebolt.common.urls import ACCOUNT_ENGINE_URL, ENGINE_BY_NAME_URL
+from firebolt.common.urls import ACCOUNT_ENGINE_BY_NAME_URL, ACCOUNT_ENGINE_URL
 from firebolt.common.util import fix_url_schema
 
 DEFAULT_TIMEOUT_SECONDS: int = 5
 
 
 async def _resolve_engine_url(
-    engine_name: str, username: str, password: str, api_endpoint: str
+    engine_name: str,
+    username: str,
+    password: str,
+    api_endpoint: str,
+    account_name: Optional[str] = None,
 ) -> str:
     async with AsyncClient(
         auth=(username, password),
         base_url=api_endpoint,
+        account_name=account_name,
         api_endpoint=api_endpoint,
     ) as client:
         try:
+            account_id = await client.account_id
             response = await client.get(
-                url=ENGINE_BY_NAME_URL,
+                url=ACCOUNT_ENGINE_BY_NAME_URL.format(account_id=account_id),
                 params={"engine_name": engine_name},
             )
             response.raise_for_status()
             engine_id = response.json()["engine_id"]["engine_id"]
-            account_id = await client.account_id
             response = await client.get(
                 url=ACCOUNT_ENGINE_URL.format(
                     account_id=account_id, engine_id=engine_id
@@ -61,6 +66,7 @@ def async_connect_factory(connection_class: Type) -> Callable:
         password: str = None,
         engine_name: Optional[str] = None,
         engine_url: Optional[str] = None,
+        account_name: Optional[str] = None,
         api_endpoint: str = DEFAULT_API_URL,
     ) -> Connection:
         """
@@ -72,6 +78,8 @@ def async_connect_factory(connection_class: Type) -> Callable:
             password: password to use for authentication
             engine_name: Optional The name of the engine to connect to
             engine_url: Optional. The engine endpoint to use
+            account_name: For customers with multiple accounts; if None uses default.
+            api_endpoint(optional): Firebolt API endpoint. Used for authentication.
 
         Note:
             either `engine_name` or `engine_url` should be provided, but not both
@@ -108,10 +116,11 @@ def async_connect_factory(connection_class: Type) -> Callable:
 
         if engine_name:
             engine_url = await _resolve_engine_url(
-                engine_name,
-                username,
-                password,
-                api_endpoint,
+                engine_name=engine_name,
+                username=username,
+                password=password,
+                account_name=account_name,
+                api_endpoint=api_endpoint,
             )
 
         assert engine_url is not None
@@ -210,7 +219,7 @@ class Connection(BaseConnection):
         database: Firebolt database name
         username: Firebolt account username
         password: Firebolt account password
-        api_endpoint: Optional. Firebolt API endpoint. Used for authentication
+        api_endpoint: Optional. Firebolt API endpoint. Used for authentication.
 
     Note:
         Firebolt currenly doesn't support transactions

src/firebolt/client/client.py
@@ -4,11 +4,13 @@ from async_property import async_cached_property  # type: ignore
 from httpx import AsyncClient as HttpxAsyncClient
 from httpx import Client as HttpxClient
 from httpx import _types
+from httpx import codes as HttpxCodes
 from httpx._types import AuthTypes
 
 from firebolt.client.auth import Auth
 from firebolt.client.constants import DEFAULT_API_URL
-from firebolt.common.urls import ACCOUNT_URL
+from firebolt.common.exception import AccountNotFoundError
+from firebolt.common.urls import ACCOUNT_BY_NAME_URL, ACCOUNT_URL
 from firebolt.common.util import cached_property, fix_url_schema, mixin_for
 
 FireboltClientMixinBase = mixin_for(HttpxClient)  # type: Any
@@ -18,10 +20,12 @@ class FireboltClientMixin(FireboltClientMixinBase):
     def __init__(
         self,
         *args: Any,
+        account_name: Optional[str] = None,
         api_endpoint: str = DEFAULT_API_URL,
         auth: AuthTypes = None,
         **kwargs: Any,
     ):
+        self.account_name = account_name
         self._api_endpoint = fix_url_schema(api_endpoint)
         super().__init__(*args, auth=auth, **kwargs)
 
@@ -54,7 +58,17 @@ class Client(FireboltClientMixin, HttpxClient):
 
     @cached_property
     def account_id(self) -> str:
-        return self.get(url=ACCOUNT_URL).json()["account"]["id"]
+        if self.account_name:
+            response = self.get(
+                url=ACCOUNT_BY_NAME_URL, params={"account_name": self.account_name}
+            )
+            if response.status_code == HttpxCodes.NOT_FOUND:
+                raise AccountNotFoundError(self.account_name)
+            # process all other status codes
+            response.raise_for_status()
+            return response.json()["account_id"]
+        else:  # account_name isn't set, use the default account.
+            return self.get(url=ACCOUNT_URL).json()["account"]["id"]
 
 
 class AsyncClient(FireboltClientMixin, HttpxAsyncClient):
@@ -72,4 +86,14 @@ class AsyncClient(FireboltClientMixin, HttpxAsyncClient):
 
     @async_cached_property
     async def account_id(self) -> str:
-        return (await self.get(url=ACCOUNT_URL)).json()["account"]["id"]
+        if self.account_name:
+            response = await self.get(
+                url=ACCOUNT_BY_NAME_URL, params={"account_name": self.account_name}
+            )
+            if response.status_code == HttpxCodes.NOT_FOUND:
+                raise AccountNotFoundError(self.account_name)
+            # process all other status codes
+            response.raise_for_status()
+            return response.json()["account_id"]
+        else:  # account_name isn't set; use the default account.
+            return (await self.get(url=ACCOUNT_URL)).json()["account"]["id"]

src/firebolt/common/exception.py
@@ -29,6 +29,14 @@ class FireboltDatabaseError(FireboltError):
     pass
 
 
+class AccountNotFoundError(FireboltError):
+    def __init__(self, method_name: str):
+        self.method_name = method_name
+
+    def __str__(self) -> str:
+        return f'Account "{self.method_name}" does not exist.'
+
+
 class AttachedEngineInUseError(FireboltDatabaseError):
     def __init__(self, method_name: str):
         self.method_name = method_name
@@ -99,7 +107,6 @@ Error = FireboltError
 
 
 class InterfaceError(Error):
-
     """
     Exception raised for errors that are related to the database interface
     rather than the database itself.

src/firebolt/common/settings.py
@@ -2,6 +2,7 @@ from pydantic import BaseSettings, Field, SecretStr
 
 
 class Settings(BaseSettings):
+    account_name: str = Field(None, env="FIREBOLT_ACCOUNT")
     server: str = Field(..., env="FIREBOLT_SERVER")
     user: str = Field(..., env="FIREBOLT_USER")
     password: SecretStr = Field(..., env="FIREBOLT_PASSWORD")

src/firebolt/common/urls.py
@@ -2,7 +2,6 @@ AUTH_URL = "{api_endpoint}/auth/v1/login"
 
 DATABASES_URL = "/core/v1/account/databases"
 
-ENGINE_BY_NAME_URL = "/core/v1/account/engines:getIdByName"
 ENGINES_URL = "/core/v1/account/engines"
 ENGINES_BY_IDS_URL = "/core/v1/engines:getByIds"
 

src/firebolt/db/connection.py
@@ -26,7 +26,7 @@ class Connection(AsyncBaseConnection):
         database: Firebolt database name
         username: Firebolt account username
         password: Firebolt account password
-        api_endpoint: Optional. Firebolt API endpoint. Used for authentication
+        api_endpoint: Optional. Firebolt API endpoint. Used for authentication.
 
     Note:
         Firebolt currenly doesn't support transactions so commit and rollback methods

src/firebolt/service/manager.py
@@ -4,7 +4,6 @@ from httpx import Timeout
 
 from firebolt.client import Client, log_request, log_response, raise_on_4xx_5xx
 from firebolt.common import Settings
-from firebolt.common.urls import ACCOUNT_BY_NAME_URL
 from firebolt.service.provider import get_provider_id
 
 DEFAULT_TIMEOUT_SECONDS: int = 60 * 2
@@ -34,6 +33,7 @@ class ResourceManager:
         self.client = Client(
             auth=(self.settings.user, self.settings.password.get_secret_value()),
             base_url=f"https://{ self.settings.server}",
+            account_name=self.settings.account_name,
             api_endpoint=self.settings.server,
             timeout=Timeout(DEFAULT_TIMEOUT_SECONDS),
         )
@@ -42,7 +42,7 @@ class ResourceManager:
             "response": [raise_on_4xx_5xx, log_response],
         }
 
-        self.account_id = self._get_account_id(account_name=account_name)
+        self.account_id = self.client.account_id
         self._init_services()
 
     def _init_services(self) -> None:
@@ -64,17 +64,3 @@ class ResourceManager:
         self.engines = EngineService(resource_manager=self)
         self.engine_revisions = EngineRevisionService(resource_manager=self)
         self.bindings = BindingService(resource_manager=self)
-
-    def _get_account_id(self, account_name: Optional[str]) -> str:
-        """
-        Given account_name, look up account_id. If account_name is None,
-        get the default account_id.
-
-        Args:
-            account_name: Name of the account.
-        """
-        if account_name is None:
-            return self.client.account_id
-        return self.client.get(
-            url=ACCOUNT_BY_NAME_URL, params={"account_name": account_name.lower()}
-        ).json()["account_id"]

tests/integration/dbapi/async/conftest.py
@@ -5,13 +5,19 @@ from firebolt.async_db import Connection, connect
 
 @fixture
 async def connection(
-    engine_url: str, database_name: str, username: str, password: str, api_endpoint: str
+    engine_url: str,
+    database_name: str,
+    username: str,
+    password: str,
+    account_name: str,
+    api_endpoint: str,
 ) -> Connection:
     async with await connect(
         engine_url=engine_url,
         database=database_name,
         username=username,
         password=password,
+        account_name=account_name,
         api_endpoint=api_endpoint,
     ) as connection:
         yield connection
@@ -23,6 +29,7 @@ async def connection_engine_name(
     database_name: str,
     username: str,
     password: str,
+    account_name: str,
     api_endpoint: str,
 ) -> Connection:
 
@@ -31,6 +38,7 @@ async def connection_engine_name(
         database=database_name,
         username=username,
         password=password,
+        account_name=account_name,
         api_endpoint=api_endpoint,
     ) as connection:
         yield connection

tests/integration/dbapi/async/test_errors_async.py
@@ -3,6 +3,7 @@ from pytest import mark, raises
 
 from firebolt.async_db import Connection, connect
 from firebolt.common.exception import (
+    AccountNotFoundError,
     AuthenticationError,
     EngineNotRunningError,
     FireboltDatabaseError,
@@ -28,12 +29,43 @@ async def test_invalid_credentials(
 
         assert str(exc_info.value).startswith(
             "Failed to authenticate"
-        ), "Invalid authentication error message"
+        ), "Invalid authentication error message."
+
+
+@mark.asyncio
+async def test_invalid_account(
+    database_name: str,
+    engine_name: str,
+    username: str,
+    password: str,
+    api_endpoint: str,
+) -> None:
+    """Connection properly reacts to invalid account error."""
+    account_name = "--"
+    with raises(AccountNotFoundError) as exc_info:
+        async with await connect(
+            database=database_name,
+            engine_name=engine_name,  # Omit engine_url to force account_id lookup.
+            username=username,
+            password=password,
+            account_name=account_name,
+            api_endpoint=api_endpoint,
+        ) as connection:
+            await connection.cursor().execute("show tables")
+
+        assert str(exc_info.value).startswith(
+            f'Account "{account_name}" does not exist'
+        ), "Invalid account error message."
 
 
 @mark.asyncio
 async def test_engine_url_not_exists(
-    engine_url: str, database_name: str, username: str, password: str, api_endpoint: str
+    engine_url: str,
+    database_name: str,
+    username: str,
+    password: str,
+    account_name: str,
+    api_endpoint: str,
 ) -> None:
     """Connection properly reacts to invalid engine url error"""
     async with await connect(
@@ -41,6 +73,7 @@ async def test_engine_url_not_exists(
         database=database_name,
         username=username,
         password=password,
+        account_name=account_name,
         api_endpoint=api_endpoint,
     ) as connection:
         with raises(ConnectError):
@@ -53,6 +86,7 @@ async def test_engine_name_not_exists(
     database_name: str,
     username: str,
     password: str,
+    account_name: str,
     api_endpoint: str,
 ) -> None:
     """Connection properly reacts to invalid engine name error"""
@@ -62,6 +96,7 @@ async def test_engine_name_not_exists(
             database=database_name,
             username=username,
             password=password,
+            account_name=account_name,
             api_endpoint=api_endpoint,
         ) as connection:
             await connection.cursor().execute("show tables")
@@ -73,6 +108,7 @@ async def test_engine_stopped(
     database_name: str,
     username: str,
     password: str,
+    account_name: str,
     api_endpoint: str,
 ) -> None:
     """Connection properly reacts to invalid engine name error"""
@@ -82,6 +118,7 @@ async def test_engine_stopped(
             database=database_name,
             username=username,
             password=password,
+            account_name=account_name,
             api_endpoint=api_endpoint,
         ) as connection:
             await connection.cursor().execute("show tables")

tests/integration/dbapi/conftest.py
@@ -18,6 +18,7 @@ STOPPED_ENGINE_NAME_ENV = "STOPPED_ENGINE_NAME"
 DATABASE_NAME_ENV = "DATABASE_NAME"
 USER_NAME_ENV = "USER_NAME"
 PASSWORD_ENV = "PASSWORD"
+ACCOUNT_NAME_ENV = "ACCOUNT_NAME"
 API_ENDPOINT_ENV = "API_ENDPOINT"
 
 
@@ -60,6 +61,11 @@ def username() -> str:
 @fixture(scope="session")
 def password() -> str:
     return must_env(PASSWORD_ENV)
+
+
+@fixture(scope="session")
+def account_name() -> str:
+    return must_env(ACCOUNT_NAME_ENV)
 
 
 @fixture(scope="session")

tests/integration/dbapi/sync/conftest.py
@@ -5,13 +5,19 @@ from firebolt.db import Connection, connect
 
 @fixture
 def connection(
-    engine_url: str, database_name: str, username: str, password: str, api_endpoint: str
+    engine_url: str,
+    database_name: str,
+    username: str,
+    password: str,
+    account_name: str,
+    api_endpoint: str,
 ) -> Connection:
     connection = connect(
         engine_url=engine_url,
         database=database_name,
         username=username,
         password=password,
+        account_name=account_name,
         api_endpoint=api_endpoint,
     )
     yield connection
@@ -24,6 +30,7 @@ def connection_engine_name(
     database_name: str,
     username: str,
     password: str,
+    account_name: str,
     api_endpoint: str,
 ) -> Connection:
     connection = connect(
@@ -31,6 +38,7 @@ def connection_engine_name(
         database=database_name,
         username=username,
         password=password,
+        account_name=account_name,
         api_endpoint=api_endpoint,
     )
     yield connection

tests/integration/dbapi/sync/test_errors.py
@@ -2,6 +2,7 @@ from httpx import ConnectError
 from pytest import raises
 
 from firebolt.common.exception import (
+    AccountNotFoundError,
     AuthenticationError,
     EngineNotRunningError,
     FireboltDatabaseError,
@@ -28,6 +29,31 @@ def test_invalid_credentials(
         assert str(exc_info.value).startswith(
             "Failed to authenticate"
         ), "Invalid authentication error message"
+
+
+def test_invalid_account(
+    database_name: str,
+    engine_name: str,
+    username: str,
+    password: str,
+    api_endpoint: str,
+) -> None:
+    """Connection properly reacts to invalid account error."""
+    account_name = "--"
+    with raises(AccountNotFoundError) as exc_info:
+        with connect(
+            database=database_name,
+            engine_name=engine_name,  # Omit engine_url to force account_id lookup.
+            username=username,
+            password=password,
+            account_name=account_name,
+            api_endpoint=api_endpoint,
+        ) as connection:
+            connection.cursor().execute("show tables")
+
+        assert str(exc_info.value).startswith(
+            f'Account "{account_name}" does not exist'
+        ), "Invalid account error message."
 
 
 def test_engine_url_not_exists(

tests/unit/async_db/conftest.py
@@ -193,6 +193,7 @@ async def connection(settings: Settings, db_name: str) -> Connection:
             database=db_name,
             username="u",
             password="p",
+            account_name=settings.account_name,
             api_endpoint=settings.server,
         )
     ) as connection:

tests/unit/async_db/test_connection.py
@@ -12,7 +12,7 @@ from firebolt.common.exception import (
     InterfaceError,
 )
 from firebolt.common.settings import Settings
-from firebolt.common.urls import ENGINE_BY_NAME_URL
+from firebolt.common.urls import ACCOUNT_ENGINE_BY_NAME_URL
 
 
 @mark.asyncio
@@ -68,6 +68,7 @@ async def test_cursor_initialized(
                 database=db_name,
                 username="u",
                 password="p",
+                account_name="a",
                 api_endpoint=settings.server,
             )
         ) as connection:
@@ -131,6 +132,7 @@ async def test_connect_engine_name(
             database="db",
             username="username",
             password="password",
+            account_name="account_name",
         ):
             pass
     assert str(exc_info.value).startswith(
@@ -142,6 +144,7 @@ async def test_connect_engine_name(
             database="db",
             username="username",
             password="password",
+            account_name="account",
         ):
             pass
     assert str(exc_info.value).startswith(
@@ -158,7 +161,7 @@ async def test_connect_engine_name(
     # Mock engine id lookup error
     httpx_mock.add_response(
         url=f"https://{settings.server}"
-        + ENGINE_BY_NAME_URL
+        + ACCOUNT_ENGINE_BY_NAME_URL.format(account_id=account_id)
         + f"?engine_name={engine_name}",
         status_code=codes.NOT_FOUND,
     )
@@ -169,6 +172,7 @@ async def test_connect_engine_name(
             username="username",
             password="password",
             engine_name=engine_name,
+            account_name=settings.account_name,
             api_endpoint=settings.server,
         ):
             pass
@@ -176,7 +180,7 @@ async def test_connect_engine_name(
     # Mock engine id lookup by name
     httpx_mock.add_response(
         url=f"https://{settings.server}"
-        + ENGINE_BY_NAME_URL
+        + ACCOUNT_ENGINE_BY_NAME_URL.format(account_id=account_id)
         + f"?engine_name={engine_name}",
         status_code=codes.OK,
         json={"engine_id": {"engine_id": engine_id}},
@@ -187,6 +191,7 @@ async def test_connect_engine_name(
         database=db_name,
         username="u",
         password="p",
+        account_name=settings.account_name,
         api_endpoint=settings.server,
     ) as connection:
         assert await connection.cursor().execute("select*") == len(python_query_data)

tests/unit/client/test_client_async.py
@@ -112,4 +112,4 @@ async def test_client_account_id(
         base_url=fix_url_schema(settings.server),
         api_endpoint=settings.server,
     ) as c:
-        assert await c.account_id == account_id, "Invalid account id returned"
+        assert await c.account_id == account_id, "Invalid account id returned."

tests/unit/conftest.py
@@ -20,6 +20,7 @@ from firebolt.common.exception import (
 )
 from firebolt.common.settings import Settings
 from firebolt.common.urls import (
+    ACCOUNT_BY_NAME_URL,
     ACCOUNT_ENGINE_URL,
     ACCOUNT_URL,
     AUTH_URL,
@@ -94,6 +95,7 @@ def settings(server, region_1) -> Settings:
         user="email@domain.com",
         password=SecretStr("*****"),
         default_region=region_1.name,
+        account_name=None,
     )
 
 
@@ -124,19 +126,30 @@ def db_name() -> str:
 
 @pytest.fixture
 def account_id_url(settings: Settings) -> str:
-    return f"https://{settings.server}{ACCOUNT_URL}"
+    if not settings.account_name:  # if None or ''
+        return f"https://{settings.server}{ACCOUNT_URL}"
+    else:
+        return (
+            f"https://{settings.server}{ACCOUNT_BY_NAME_URL}"
+            f"?account_name={settings.account_name}"
+        )
 
 
 @pytest.fixture
-def account_id_callback(account_id: str, account_id_url: str) -> Callable:
+def account_id_callback(
+    account_id: str, account_id_url: str, settings: Settings
+) -> Callable:
     def do_mock(
         request: httpx.Request = None,
         **kwargs,
     ) -> Response:
         assert request.url == account_id_url
-        return to_response(
-            status_code=httpx.codes.OK, json={"account": {"id": account_id}}
-        )
+        if account_id_url.endswith(ACCOUNT_URL):  # account_name shouldn't be specified.
+            return to_response(
+                status_code=httpx.codes.OK, json={"account": {"id": account_id}}
+            )
+        # In this case, an account_name *should* be specified.
+        return to_response(status_code=httpx.codes.OK, json={"account_id": account_id})
 
     return do_mock
 

tests/unit/db/test_connection.py
@@ -7,7 +7,7 @@ from pytest_httpx import HTTPXMock
 from firebolt.async_db._types import ColType
 from firebolt.common.exception import ConnectionClosedError, InterfaceError
 from firebolt.common.settings import Settings
-from firebolt.common.urls import ENGINE_BY_NAME_URL
+from firebolt.common.urls import ACCOUNT_ENGINE_BY_NAME_URL
 from firebolt.db import Connection, connect
 
 
@@ -144,7 +144,7 @@ def test_connect_engine_name(
     # Mock engine id lookup by name
     httpx_mock.add_response(
         url=f"https://{settings.server}"
-        + ENGINE_BY_NAME_URL
+        + ACCOUNT_ENGINE_BY_NAME_URL.format(account_id=account_id)
         + f"?engine_name={engine_name}",
         status_code=codes.OK,
         json={"engine_id": {"engine_id": engine_id}},
@@ -155,6 +155,7 @@ def test_connect_engine_name(
         database=db_name,
         username="u",
         password="p",
+        account_name=settings.account_name,
         api_endpoint=settings.server,
     ) as connection:
         assert connection.cursor().execute("select*") == len(python_query_data)