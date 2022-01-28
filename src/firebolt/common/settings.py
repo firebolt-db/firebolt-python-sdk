2948c534ed57e47a321e09d41af716a5fb40cdd3
Stepan Burlakov <stepansergeevitch@gmail.com>
2022-01-28 17:39:51 +0200

    add auth_token settings field

--- src/firebolt/common/settings.py
+++ src/firebolt/common/settings.py
@@ -1,12 +1,25 @@
-from pydantic import BaseSettings, Field, SecretStr
+from pydantic import BaseSettings, Field, SecretStr, root_validator
 
 
 class Settings(BaseSettings):
+    # Authorization
+    user: str = Field(None, env="FIREBOLT_USER")
+    password: SecretStr = Field(None, env="FIREBOLT_PASSWORD")
+    # Or
+    auth_token: str = Field(None, env="FIREBOLT_AUTH_TOKEN")
+
     account_name: str = Field(None, env="FIREBOLT_ACCOUNT")
     server: str = Field(..., env="FIREBOLT_SERVER")
-    user: str = Field(..., env="FIREBOLT_USER")
-    password: SecretStr = Field(..., env="FIREBOLT_PASSWORD")
     default_region: str = Field(..., env="FIREBOLT_DEFAULT_REGION")
 
     class Config:
         env_file = ".env"
+
+    @root_validator
+    def mutual_exclusive_with_creds(cls, values: dict) -> dict:
+        if values["user"] or values["password"]:
+            if values["auth_token"]:
+                raise ValueError("Provide only one of user/password or auth_token")
+        elif not values["auth_token"]:
+            raise ValueError("Provide either user/password or auth_token")
+        return values
---
+++ tests/unit/common/test_settings.py
@@ -0,0 +1,49 @@
+from typing import Tuple
+
+from pydantic import ValidationError
+from pytest import mark, raises
+
+from firebolt.common.settings import Settings
+
+
+@mark.parametrize(
+    "fields",
+    (
+        ("user", "password", "account_name", "server", "default_region"),
+        ("auth_token", "account_name", "server", "default_region"),
+    ),
+)
+def test_settings_happy_path(fields: Tuple[str]) -> None:
+    kwargs = {f: f for f in fields}
+    s = Settings(**kwargs)
+
+    for f in fields:
+        field = getattr(s, f)
+        if hasattr(field, "get_secret_value"):
+            field = field.get_secret_value()
+        assert field == f, f"Invalid settings value {f}"
+
+
+def test_settings_auth_credentials() -> None:
+    creds = ("auth_token", "user", "password")
+    other = ("server", "default_region")
+
+    kwargs = {f: f for f in other}
+    with raises(ValidationError) as exc_info:
+        Settings(**kwargs)
+
+    err = exc_info.value
+    assert (
+        len(err.errors()) > 0
+        and err.errors()[0]["msg"] == "Provide either user/password or auth_token"
+    ), "Invalid error message"
+
+    kwargs = {f: f for f in creds + other}
+    with raises(ValidationError) as exc_info:
+        Settings(**kwargs)
+
+    err = exc_info.value
+    assert (
+        len(err.errors()) > 0
+        and err.errors()[0]["msg"] == "Provide only one of user/password or auth_token"
+    ), "Invalid error message"
