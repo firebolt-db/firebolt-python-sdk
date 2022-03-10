bc8503de12528c9b2d3fe2fdf5e6f314ccbfd99c
Stepan Burlakov <stepan.burlakov@northspyre.com>
2022-03-10 10:24:52 +0100

    extend unit tests

--- tests/integration/dbapi/conftest.py
+++ tests/integration/dbapi/conftest.py
@@ -7,7 +7,7 @@ from pytest import fixture
 
 from firebolt.async_db._types import ColType
 from firebolt.async_db.cursor import Column
-from firebolt.db import ARRAY, DECIMAL
+from firebolt.db import ARRAY, DATETIME64, DECIMAL
 
 LOGGER = getLogger(__name__)
 
@@ -15,12 +15,25 @@ LOGGER = getLogger(__name__)
 @fixture
 def all_types_query() -> str:
     return (
-        "select 1 as uint8, -1 as int8, 257 as uint16, -257 as int16, 80000 as uint32, "
-        "-80000 as int32, 30000000000 as uint64, -30000000000 as int64, cast(1.23 AS "
-        "FLOAT) as float32, 1.2345678901234 as float64, 'text' as \"string\", "
-        "CAST('2021-03-28' AS DATE) as \"date\", CAST('2019-07-31 01:01:01' AS "
-        'DATETIME) as "datetime", true as "bool",[1,2,3,4] as "array", '
-        'cast(1231232.12 as decimal(38,2)) as "decimal", cast(null as  int) as nullable'
+        "select 1 as uint8, "
+        "-1 as int8, "
+        "257 as uint16, "
+        "-257 as int16, "
+        "80000 as uint32, "
+        "-80000 as int32, "
+        "30000000000 as uint64, "
+        "-30000000000 as int64, "
+        "cast(1.23 AS FLOAT) as float32, "
+        "1.2345678901234 as float64, "
+        "'text' as \"string\", "
+        "CAST('2021-03-28' AS DATE) as \"date\", "
+        "CAST('1860-03-04' AS DATE_EXT) as \"date32\","
+        "CAST('2019-07-31 01:01:01' AS DATETIME) as \"datetime\", "
+        "CAST('2019-07-31 01:01:01.1234' AS TIMESTAMP_EXT(4)) as \"datetime64\", "
+        'true as "bool",'
+        '[1,2,3,4] as "array", cast(1231232.123459999990457054844258706536 as '
+        'decimal(38,30)) as "decimal", '
+        "cast(null as  int) as nullable"
     )
 
 
@@ -39,10 +52,12 @@ def all_types_query_description() -> List[Column]:
         Column("float64", float, None, None, None, None, None),
         Column("string", str, None, None, None, None, None),
         Column("date", date, None, None, None, None, None),
+        Column("date32", date, None, None, None, None, None),
         Column("datetime", datetime, None, None, None, None, None),
+        Column("datetime64", DATETIME64(4), None, None, None, None, None),
         Column("bool", int, None, None, None, None, None),
         Column("array", ARRAY(int), None, None, None, None, None),
-        Column("decimal", DECIMAL(38, 2), None, None, None, None, None),
+        Column("decimal", DECIMAL(38, 30), None, None, None, None, None),
         Column("nullable", int, None, None, None, None, None),
     ]
 
@@ -63,10 +78,12 @@ def all_types_query_response() -> List[ColType]:
             1.23456789012,
             "text",
             date(2021, 3, 28),
+            date(1860, 3, 4),
             datetime(2019, 7, 31, 1, 1, 1),
+            datetime(2019, 7, 31, 1, 1, 1, 123400),
             1,
             [1, 2, 3, 4],
-            Decimal("1231232.12"),
+            Decimal("1231232.123459999990457054844258706536"),
             None,
         ]
     ]
