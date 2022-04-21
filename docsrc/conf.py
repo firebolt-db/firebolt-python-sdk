d64dd91ad2ae8a79889cee01fab5ad28d889d311
marknoack <88755261+marknoack@users.noreply.github.com>
2022-04-21 15:00:55 -0700

    Update conf.py
    formatted bracket

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -41,7 +41,7 @@ extensions = [
     "sphinx.ext.autodoc", 
     "sphinx.ext.napoleon", 
     "sphinx.ext.autosectionlabel" 
-    ]
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
