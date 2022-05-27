30c875a1758b31e21548c9cead16522a12d65be1
Stepan Burlakov <stepan.burlakov@firebolt.io>
2022-05-27 12:24:06 +0200

    implement auth locking

--- src/firebolt/client/auth/base.py
+++ src/firebolt/client/auth/base.py
@@ -1,5 +1,6 @@
+from asyncio import Lock as ALock
 from time import time
-from typing import Generator, Optional
+from typing import AsyncGenerator, Generator, Optional
 
 from httpx import Auth as HttpxAuth
 from httpx import Request, Response, codes
@@ -35,6 +36,7 @@ class Auth(HttpxAuth):
         self._use_token_cache = use_token_cache
         self._token: Optional[str] = self._get_cached_token()
         self._expires: Optional[int] = None
+        self._async_auth_lock = ALock()
 
     def copy(self) -> "Auth":
         """Make another auth object with same credentials.
@@ -95,6 +97,7 @@ class Auth(HttpxAuth):
 
     def get_new_token_generator(self) -> Generator[Request, Response, None]:
         """Generate requests needed to create a new token session."""
+        raise NotImplementedError()
 
     def auth_flow(self, request: Request) -> Generator[Request, Response, None]:
         """Add authorization token to request headers.
@@ -119,3 +122,90 @@ class Auth(HttpxAuth):
             yield from self.get_new_token_generator()
             request.headers["Authorization"] = f"Bearer {self.token}"
             yield request
+
+    def sync_authentication(
+        self, force: bool = False
+    ) -> Generator[Request, Response, None]:
+        if not self.token or self.expired or force:
+            flow = self.get_new_token_generator()
+            response = None
+            while True:
+                try:
+                    request = (
+                        flow.send(response) if response else next(flow)  # type: ignore
+                    )
+                except StopIteration:
+                    break
+                response = yield request
+                response.read()
+            self._cache_token()
+
+    def sync_auth_flow(self, request: Request) -> Generator[Request, Response, None]:
+        """Execute the authentication flow synchronously.
+
+        Sycnronously fetches the new token in these cases:
+         - We haven't yet fetched id
+         - It's expired
+         - In case a request, authorized with previous token,
+           failed with 401 Unauthorized error. Only one attempt to reauthenticate
+           is performed, if the error persists, it will just be raised
+
+        Args:
+            request (Request): A request that should be authorized
+
+        Yields:
+            Request: Request, that should be executed for auth flow
+        """
+        yield from self.sync_authentication()
+        request.headers["Authorization"] = f"Bearer {self.token}"
+        response = yield request
+
+        if response.status_code == codes.UNAUTHORIZED:
+            yield from self.sync_authentication(force=True)
+            request.headers["Authorization"] = f"Bearer {self.token}"
+            yield request
+
+    async def async_auth_flow(
+        self, request: Request
+    ) -> AsyncGenerator[Request, Response]:
+        """Execute the authentication flow asynchronously.
+
+        Asycnronously fetches the new token in these cases:
+         - We haven't yet fetched id
+         - It's expired
+         - In case a request, authorized with previous token,
+           failed with 401 Unauthorized error. Only one attempt to reauthenticate
+           is performed, if the error persists, it will just be raised
+
+        Args:
+            request (Request): A request that should be authorized
+
+        Yields:
+            Request: Request, that should be executed for auth flow
+        """
+        response: Optional[Response] = None
+        attempts = 0
+        while (
+            response is None or response.status_code == codes.UNAUTHORIZED
+        ) and attempts < 2:
+            request_failed = response and response.status_code == codes.UNAUTHORIZED
+            async with self._async_auth_lock:
+                if not self.token or self.expired or request_failed:
+                    flow = self.get_new_token_generator()
+                    auth_response: Optional[Response] = None
+                    while True:
+                        try:
+                            auth_request = (
+                                flow.send(auth_response)
+                                if auth_response
+                                else next(flow)
+                            )
+                        except StopIteration:
+                            break
+                        auth_response = yield auth_request
+                        await auth_response.aread()
+                    self._cache_token()
+
+            request.headers["Authorization"] = f"Bearer {self.token}"
+            response = yield request
+            attempts += 1
