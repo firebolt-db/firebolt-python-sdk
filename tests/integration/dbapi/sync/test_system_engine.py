cd269f6ef7ee746d61412e26adc9f24bfc9dab47
ptiurin <petro.tiurin@firebolt.io>
2022-11-28 19:28:58 +0000

    refactor to individual test cases

--- tests/integration/dbapi/sync/test_system_engine.py
+++ tests/integration/dbapi/sync/test_system_engine.py
@@ -9,6 +9,11 @@ def db_name():
 
 
 @fixture
+def second_db_name():
+    return "ecosystem_test_db_two"
+
+
+@fixture
 def region():
     return "us-east-1"
 
@@ -41,26 +46,37 @@ def engine_specs(region):
 
 
 def create_database(name, specs=None):
-    query = f"CREATE DATABASE IF NOT EXISTS {name}"
+    query = f"CREATE DATABASE {name}"
     query += f" WITH {specs}" if specs else ""
     return query
 
 
 def create_engine(name, specs=None):
-    query = f"CREATE ENGINE IF NOT EXISTS {name}"
+    query = f"CREATE ENGINE {name}"
     query += f" WITH {specs}" if specs else ""
     return query
 
 
-def test_system_database(connection_system_engine, db_name, engine_name, region):
+@fixture
+def setup_dbs(connection_system_engine, db_name, second_db_name, engine_name, region):
     with connection_system_engine.cursor() as cursor:
         cursor.execute(create_database(name=db_name))
 
         cursor.execute(create_engine(engine_name, engine_specs(region)))
 
         cursor.execute(
-            create_database(name=f"{db_name}_two", specs=db_specs(region, engine_name))
+            create_database(name=second_db_name, specs=db_specs(region, engine_name))
         )
+
+        yield
+
+        cursor.execute(f"DROP ENGINE IF EXISTS {engine_name}")
+        cursor.execute(f"DROP DATABASE IF EXISTS {db_name}")
+        cursor.execute(f"DROP DATABASE IF EXISTS {second_db_name}")
+
+
+def test_show_databases(setup_dbs, connection_system_engine, db_name):
+    with connection_system_engine.cursor() as cursor:
 
         cursor.execute("SHOW DATABASES")
 
@@ -69,23 +85,36 @@ def test_system_database(connection_system_engine, db_name, engine_name, region)
         assert db_name in dbs
         assert f"{db_name}_two" in dbs
 
-        cursor.execute(f"DETACH ENGINE {engine_name} FROM {db_name}_two")
+
+def test_detach_engine(
+    setup_dbs, connection_system_engine, engine_name, second_db_name
+):
+    with connection_system_engine.cursor() as cursor:
+        cursor.execute("SHOW ENGINES")
+        engines = cursor.fetchall()
+        assert engine_name in [row[0] for row in engines]
+        assert (engine_name, second_db_name) in [(row[0], row[5]) for row in engines]
+
+        cursor.execute(f"DETACH ENGINE {engine_name} FROM {second_db_name}")
 
         cursor.execute("SHOW ENGINES")
         engines = cursor.fetchall()
         assert engine_name in [row[0] for row in engines]
         assert (engine_name, "-") in [(row[0], row[5]) for row in engines]
 
-        cursor.execute(f"ATTACH ENGINE {engine_name} TO {db_name}")
+        cursor.execute(f"ATTACH ENGINE {engine_name} TO {second_db_name}")
 
+
+def test_alter_engine(setup_dbs, connection_system_engine, engine_name):
+    with connection_system_engine.cursor() as cursor:
         cursor.execute(f"ALTER ENGINE {engine_name} SET SPEC = B2")
 
         cursor.execute("SHOW ENGINES")
         engines = cursor.fetchall()
         assert (engine_name, "B2") in [(row[0], row[2]) for row in engines]
 
+
+def test_start_stop_engine(setup_dbs, connection_system_engine, engine_name):
+    with connection_system_engine.cursor() as cursor:
         cursor.execute(f"START ENGINE {engine_name}")
         cursor.execute(f"STOP ENGINE {engine_name}")
-
-        cursor.execute(f"DROP ENGINE {engine_name}")
-        cursor.execute(f"DROP DATABASE {db_name}")
