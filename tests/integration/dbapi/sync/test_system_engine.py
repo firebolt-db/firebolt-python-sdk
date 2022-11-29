873c6db99259192a555923bebf25b625cf77b695
ptiurin <petro.tiurin@firebolt.io>
2022-11-29 10:29:03 +0000

    Refactor

--- tests/integration/dbapi/sync/test_system_engine.py
+++ tests/integration/dbapi/sync/test_system_engine.py
@@ -4,13 +4,13 @@ from firebolt.utils.exception import OperationalError
 
 
 @fixture
-def db_name():
-    return "ecosystem_test_db"
+def db_name(database_name):
+    return database_name + "_system_test"
 
 
 @fixture
-def second_db_name():
-    return "ecosystem_test_db_two"
+def second_db_name(database_name):
+    return database_name + "_system_test_two"
 
 
 @fixture
@@ -19,42 +19,8 @@ def region():
 
 
 @fixture
-def engine_name():
-    return "ecosystem_test_engine"
-
-
-@mark.parametrize(
-    "query",
-    ["SELECT 1", "CREATE DIMENSION TABLE dummy(id INT)", "SHOW TABLES", "SHOW INDEXES"],
-)
-def test_query_errors(connection_system_engine, query):
-    with connection_system_engine.cursor() as cursor:
-        with raises(OperationalError):
-            cursor.execute(query)
-
-
-def db_specs(region, attached_engine):
-    return (
-        f"REGION = '{region}' "
-        f"ATTACHED_ENGINES = ('{attached_engine}') "
-        "DESCRIPTION = 'Sample description'"
-    )
-
-
-def engine_specs(region):
-    return f"REGION = '{region}' " "SPEC = 'B1' " "SCALE = 1"
-
-
-def create_database(name, specs=None):
-    query = f"CREATE DATABASE {name}"
-    query += f" WITH {specs}" if specs else ""
-    return query
-
-
-def create_engine(name, specs=None):
-    query = f"CREATE ENGINE {name}"
-    query += f" WITH {specs}" if specs else ""
-    return query
+def engine_name(engine_name):
+    return engine_name + "_system_test"
 
 
 @fixture
@@ -75,6 +41,40 @@ def setup_dbs(connection_system_engine, db_name, second_db_name, engine_name, re
         cursor.execute(f"DROP DATABASE IF EXISTS {second_db_name}")
 
 
+def engine_specs(region):
+    return f"REGION = '{region}' " "SPEC = 'B1' " "SCALE = 1"
+
+
+def create_database(name, specs=None):
+    query = f"CREATE DATABASE {name}"
+    query += f" WITH {specs}" if specs else ""
+    return query
+
+
+def create_engine(name, specs=None):
+    query = f"CREATE ENGINE {name}"
+    query += f" WITH {specs}" if specs else ""
+    return query
+
+
+def db_specs(region, attached_engine):
+    return (
+        f"REGION = '{region}' "
+        f"ATTACHED_ENGINES = ('{attached_engine}') "
+        "DESCRIPTION = 'Sample description'"
+    )
+
+
+@mark.parametrize(
+    "query",
+    ["SELECT 1", "CREATE DIMENSION TABLE dummy(id INT)", "SHOW TABLES", "SHOW INDEXES"],
+)
+def test_query_errors(connection_system_engine, query):
+    with connection_system_engine.cursor() as cursor:
+        with raises(OperationalError):
+            cursor.execute(query)
+
+
 def test_show_databases(setup_dbs, connection_system_engine, db_name):
     with connection_system_engine.cursor() as cursor:
 
@@ -89,20 +89,23 @@ def test_show_databases(setup_dbs, connection_system_engine, db_name):
 def test_detach_engine(
     setup_dbs, connection_system_engine, engine_name, second_db_name
 ):
-    with connection_system_engine.cursor() as cursor:
+    def check_engine_exists(cursor, engine_name, db_name):
         cursor.execute("SHOW ENGINES")
         engines = cursor.fetchall()
+        # Results have the following columns
+        # engine_name, region, spec, scale, status, attached_to, version
         assert engine_name in [row[0] for row in engines]
-        assert (engine_name, second_db_name) in [(row[0], row[5]) for row in engines]
+        assert (engine_name, db_name) in [(row[0], row[5]) for row in engines]
 
+    with connection_system_engine.cursor() as cursor:
+        check_engine_exists(cursor, engine_name, db_name=second_db_name)
         cursor.execute(f"DETACH ENGINE {engine_name} FROM {second_db_name}")
 
-        cursor.execute("SHOW ENGINES")
-        engines = cursor.fetchall()
-        assert engine_name in [row[0] for row in engines]
-        assert (engine_name, "-") in [(row[0], row[5]) for row in engines]
+        # When engine not attached db is -
+        check_engine_exists(cursor, engine_name, db_name="-")
 
         cursor.execute(f"ATTACH ENGINE {engine_name} TO {second_db_name}")
+        check_engine_exists(cursor, engine_name, db_name=second_db_name)
 
 
 def test_alter_engine(setup_dbs, connection_system_engine, engine_name):
@@ -115,6 +118,17 @@ def test_alter_engine(setup_dbs, connection_system_engine, engine_name):
 
 
 def test_start_stop_engine(setup_dbs, connection_system_engine, engine_name):
+    def check_engine_status(cursor, engine_name, status):
+        cursor.execute("SHOW ENGINES")
+        engines = cursor.fetchall()
+        # Results have the following columns
+        # engine_name, region, spec, scale, status, attached_to, version
+        assert engine_name in [row[0] for row in engines]
+        assert (engine_name, status) in [(row[0], row[4]) for row in engines]
+
     with connection_system_engine.cursor() as cursor:
+        check_engine_status(cursor, engine_name, "Stopped")
         cursor.execute(f"START ENGINE {engine_name}")
+        check_engine_status(cursor, engine_name, "Running")
         cursor.execute(f"STOP ENGINE {engine_name}")
+        check_engine_status(cursor, engine_name, "Stopped")
