989e3898a0a331727041864bb5262b4e446150f3
Stepan Burlakov <stepansergeevitch@gmail.com>
2023-01-12 12:09:02 +0200

    remove select 1 from system engine errors since it's supported

--- tests/integration/dbapi/sync/test_system_engine.py
+++ tests/integration/dbapi/sync/test_system_engine.py
@@ -68,7 +68,7 @@ def db_specs(region, attached_engine):
 
 @mark.parametrize(
     "query",
-    ["SELECT 1", "CREATE DIMENSION TABLE dummy(id INT)", "SHOW TABLES", "SHOW INDEXES"],
+    ["CREATE DIMENSION TABLE dummy(id INT)", "SHOW TABLES", "SHOW INDEXES"],
 )
 def test_query_errors(connection_system_engine, query):
     with connection_system_engine.cursor() as cursor:
