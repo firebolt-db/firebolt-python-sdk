5f820136578cd5df4bd4ad4a0d8fee320c97ca5a
Stepan Burlakov <stepansergeevitch@gmail.com>
2023-02-02 12:01:29 +0200

    fix: Fir 14945 fix cached token refresh (#234)

--- tests/unit/client/test_client.py
+++ tests/unit/client/test_client.py
@@ -2,12 +2,15 @@ from re import Pattern
 from typing import Callable
 
 from httpx import codes
+from pyfakefs.fake_filesystem import FakeFilesystem
 from pytest import raises
 from pytest_httpx import HTTPXMock
 
 from firebolt.client import DEFAULT_API_URL, Client
 from firebolt.client.auth import Token, UsernamePassword
+from firebolt.client.resource_manager_hooks import raise_on_4xx_5xx
 from firebolt.common import Settings
+from firebolt.utils.token_storage import TokenSecureStorage
 from firebolt.utils.urls import AUTH_URL
 from firebolt.utils.util import fix_url_schema
 
@@ -109,3 +112,46 @@ def test_client_account_id(
         api_endpoint=settings.server,
     ) as c:
         assert c.account_id == account_id, "Invalid account id returned"
+
+
+# FIR-14945
+def test_refresh_with_hooks(
+    fs: FakeFilesystem,
+    httpx_mock: HTTPXMock,
+    test_username: str,
+    test_password: str,
+    test_token: str,
+) -> None:
+    """
+    When hooks are used, the invalid token, fetched from cache, is refreshed
+    """
+
+    tss = TokenSecureStorage(test_username, test_password)
+    tss.cache_token(test_token, 2**32)
+
+    client = Client(
+        auth=UsernamePassword(test_username, test_password),
+        event_hooks={
+            "response": [raise_on_4xx_5xx],
+        },
+    )
+
+    # client request failed authorization
+    httpx_mock.add_response(
+        status_code=codes.UNAUTHORIZED,
+    )
+
+    # auth get another token
+    httpx_mock.add_response(
+        status_code=codes.OK,
+        json={"expires_in": 2**30, "access_token": test_token},
+    )
+
+    # client request success this time
+    httpx_mock.add_response(
+        status_code=codes.OK,
+    )
+
+    assert (
+        client.get("https://url").status_code == codes.OK
+    ), "request failed with firebolt client"
