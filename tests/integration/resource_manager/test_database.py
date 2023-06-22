6f86d3cef0eed0455abe9c6fddaca2d0e38904c3
Stepan Burlakov <stepansergeevitch@gmail.com>
2023-06-22 16:10:08 +0300

    fix integration tests

--- tests/integration/resource_manager/test_database.py
+++ tests/integration/resource_manager/test_database.py
@@ -1,9 +1,11 @@
-from firebolt.common import Settings
+from firebolt.client.auth import Auth
 from firebolt.service.manager import ResourceManager
 
 
 def test_database_get_default_engine(
-    rm_settings: Settings,
+    auth: Auth,
+    account_name: str,
+    api_endpoint: str,
     database_name: str,
     stopped_engine_name: str,
     engine_name: str,
@@ -11,13 +13,64 @@ def test_database_get_default_engine(
     """
     Checks that the default engine is either running or stopped engine
     """
-    rm = ResourceManager(rm_settings)
+    rm = ResourceManager(
+        auth=auth, account_name=account_name, api_endpoint=api_endpoint
+    )
 
-    db = rm.databases.get_by_name(database_name)
+    db = rm.databases.get(database_name)
 
-    engine = db.get_default_engine()
-    assert engine is not None, "default engine is None, but shouldn't"
+    engine = db.get_attached_engines()[0]
+    assert engine is not None, "engine is None, but shouldn't be"
     assert engine.name in [
         stopped_engine_name,
         engine_name,
     ], "Returned default engine name is neither of known engines"
+
+
+def test_databases_get_many(
+    auth: Auth,
+    account_name: str,
+    api_endpoint: str,
+    database_name: str,
+    engine_name: str,
+):
+    rm = ResourceManager(
+        auth=auth, account_name=account_name, api_endpoint=api_endpoint
+    )
+
+    # get all databases, at least one should be returned
+    databases = rm.databases.get_many()
+    assert len(databases) > 0
+    assert database_name in {db.name for db in databases}
+
+    # get all databases, with name_contains
+    databases = rm.databases.get_many(name_contains=database_name)
+    assert len(databases) > 0
+    assert database_name in {db.name for db in databases}
+
+    # get all databases, with name_contains
+    databases = rm.databases.get_many(attached_engine_name_eq=engine_name)
+    assert len(databases) > 0
+    assert database_name in {db.name for db in databases}
+
+    # get all databases, with name_contains
+    databases = rm.databases.get_many(attached_engine_name_contains=engine_name)
+    assert len(databases) > 0
+    assert database_name in {db.name for db in databases}
+
+    region = [db for db in databases if db.name == database_name][0].region
+
+    # get all databases, with region_eq
+    databases = rm.databases.get_many(region_eq=region)
+    assert len(databases) > 0
+    assert database_name in {db.name for db in databases}
+
+    # get all databases, with all filters
+    databases = rm.databases.get_many(
+        name_contains=database_name,
+        attached_engine_name_eq=engine_name,
+        attached_engine_name_contains=engine_name,
+        region_eq=region,
+    )
+    assert len(databases) > 0
+    assert database_name in {db.name for db in databases}
--- tests/integration/resource_manager/test_engine.py
+++ tests/integration/resource_manager/test_engine.py
@@ -1,23 +1,24 @@
 from collections import namedtuple
 
-import pytest
-
-from firebolt.model.engine import Engine
-from firebolt.service.manager import ResourceManager, Settings
-from firebolt.service.types import (
-    EngineStatusSummary,
-    EngineType,
-    WarmupMethod,
-)
+from firebolt.client.auth import Auth
+from firebolt.service.manager import ResourceManager
+from firebolt.service.types import EngineStatus
 
 
 def make_engine_name(database_name: str, suffix: str) -> str:
     return f"{database_name}_{suffix}"
 
 
-@pytest.mark.skip(reason="manual test")
-def test_create_start_stop_engine(database_name: str):
-    rm = ResourceManager()
+# @pytest.mark.skip(reason="manual test")
+def test_create_start_stop_engine(
+    auth: Auth,
+    account_name: str,
+    api_endpoint: str,
+    database_name: str,
+):
+    rm = ResourceManager(
+        auth=auth, account_name=account_name, api_endpoint=api_endpoint
+    )
     name = make_engine_name(database_name, "start_stop")
 
     engine = rm.engines.create(name=name)
@@ -26,127 +27,77 @@ def test_create_start_stop_engine(database_name: str):
     database = rm.databases.create(name=name)
     assert database.name == name
 
-    engine.attach_to_database(database=database)
+    engine.attach_to_database(database)
     assert engine.database == database
 
-    engine = engine.start()
-    assert (
-        engine.current_status_summary
-        == EngineStatusSummary.ENGINE_STATUS_SUMMARY_RUNNING
-    )
+    engine.start()
+    assert engine.current_status == EngineStatus.RUNNING
 
-    engine = engine.stop()
-    assert engine.current_status_summary in {
-        EngineStatusSummary.ENGINE_STATUS_SUMMARY_STOPPING,
-        EngineStatusSummary.ENGINE_STATUS_SUMMARY_STOPPED,
-    }
+    engine.stop()
+    assert engine.current_status in {EngineStatus.STOPPING, EngineStatus.STOPPED}
 
-
-@pytest.mark.skip(reason="manual test")
-def test_copy_engine(database_name):
-    rm = ResourceManager()
-    name = make_engine_name(database_name, "copy")
-
-    engine = rm.engines.create(name=name)
-    assert engine.name == name
-
-    engine.name = f"{engine.name}_copy"
-    engine_copy = rm.engines._send_create_engine(
-        engine=engine,
-        engine_revision=rm.engine_revisions.get_by_key(engine.latest_revision_key),
-    )
-    assert engine_copy
-
-
-def test_databases_get_many(rm_settings: Settings, database_name, engine_name):
-    rm = ResourceManager(rm_settings)
-
-    # get all databases, at least one should be returned
-    databases = rm.databases.get_many()
-    assert len(databases) > 0
-    assert database_name in {db.name for db in databases}
-
-    # get all databases, with name_contains
-    databases = rm.databases.get_many(name_contains=database_name)
-    assert len(databases) > 0
-    assert database_name in {db.name for db in databases}
-
-    # get all databases, with name_contains
-    databases = rm.databases.get_many(attached_engine_name_eq=engine_name)
-    assert len(databases) > 0
-    assert database_name in {db.name for db in databases}
-
-    # get all databases, with name_contains
-    databases = rm.databases.get_many(attached_engine_name_contains=engine_name)
-    assert len(databases) > 0
-    assert database_name in {db.name for db in databases}
-
-
-def get_engine_params(rm: ResourceManager, engine: Engine):
-    engine_revision = rm.engine_revisions.get_by_key(engine.latest_revision_key)
-    instance_type = rm.instance_types.get_by_key(
-        engine_revision.specification.db_compute_instances_type_key
-    )
-
-    return {
-        "engine_type": engine.settings.preset,
-        "scale": engine_revision.specification.db_compute_instances_count,
-        "spec": instance_type.name,
-        "auto_stop": engine.settings.auto_stop_delay_duration,
-        "warmup": engine.settings.warm_up,
-        "description": engine.description,
-    }
+    engine.delete()
+    database.delete()
 
 
 ParamValue = namedtuple("ParamValue", "set expected")
 ENGINE_UPDATE_PARAMS = {
-    "engine_type": ParamValue(
-        EngineType.DATA_ANALYTICS, "ENGINE_SETTINGS_PRESET_DATA_ANALYTICS"
-    ),
-    "scale": ParamValue(23, 23),
-    "spec": ParamValue("B1", "B1"),
-    "auto_stop": ParamValue(123, "7380s"),
-    "warmup": ParamValue(WarmupMethod.PRELOAD_ALL_DATA, "ENGINE_SETTINGS_WARM_UP_ALL"),
-    "description": ParamValue("new db description", "new db description"),
+    # commented parameters are not available yet
+    #    "scale": ParamValue(23, 23),
+    #    "spec": ParamValue("B1", "B1"),
+    "auto_stop": ParamValue(123, 7380),
+    #    "warmup": ParamValue(WarmupMethod.PRELOAD_ALL_DATA, WarmupMethod.PRELOAD_ALL_DATA),
 }
 
 
-def test_engine_update_single_parameter(rm_settings: Settings, database_name: str):
-    rm = ResourceManager(rm_settings)
+def test_engine_update_single_parameter(
+    auth: Auth,
+    account_name: str,
+    api_endpoint: str,
+    database_name: str,
+):
+    rm = ResourceManager(
+        auth=auth, account_name=account_name, api_endpoint=api_endpoint
+    )
 
     name = make_engine_name(database_name, "single_param")
     engine = rm.engines.create(name=name)
 
-    engine.attach_to_database(database=rm.databases.get_by_name(database_name))
+    engine.attach_to_database(rm.databases.get(database_name))
     assert engine.database.name == database_name
 
     for param, value in ENGINE_UPDATE_PARAMS.items():
         engine.update(**{param: value.set})
 
-        engine = rm.engines.get_by_name(name)
-        new_params = get_engine_params(rm, engine)
-        assert new_params[param] == value.expected
+        engine_new = rm.engines.get(name)
+        assert getattr(engine_new, param) == value.expected, f"Invalid {param} value"
 
     engine.delete()
 
 
-def test_engine_update_multiple_parameters(rm_settings: Settings, database_name: str):
-    rm = ResourceManager(rm_settings)
+def test_engine_update_multiple_parameters(
+    auth: Auth,
+    account_name: str,
+    api_endpoint: str,
+    database_name: str,
+):
+    rm = ResourceManager(
+        auth=auth, account_name=account_name, api_endpoint=api_endpoint
+    )
 
     name = make_engine_name(database_name, "multi_param")
     engine = rm.engines.create(name=name)
 
-    engine.attach_to_database(database=rm.databases.get_by_name(database_name))
+    engine.attach_to_database(rm.databases.get(database_name))
     assert engine.database.name == database_name
 
     engine.update(
         **dict({(param, value.set) for param, value in ENGINE_UPDATE_PARAMS.items()})
     )
 
-    engine = rm.engines.get_by_name(name)
-    new_params = get_engine_params(rm, engine)
+    engine_new = rm.engines.get(name)
 
     for param, value in ENGINE_UPDATE_PARAMS.items():
-        assert new_params[param] == value.expected
+        assert getattr(engine_new, param) == value.expected, f"Invalid {param} value"
 
     engine.delete()
