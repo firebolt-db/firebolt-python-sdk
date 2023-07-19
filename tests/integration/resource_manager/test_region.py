68a438d7d65f3c147d7b86b8befbd98c4c67ba4f
RotemFB <rotem.bitner@firebolt.io>
2023-07-19 15:23:00 +0300

    added settings to the integration

--- tests/integration/resource_manager/test_region.py
+++ tests/integration/resource_manager/test_region.py
@@ -1,6 +1,6 @@
-from firebolt.service.manager import ResourceManager
+from firebolt.service.manager import ResourceManager, Settings
 
 
-def test_get_region():
-    rm = ResourceManager()
+def test_get_region(rm_settings: Settings):
+    rm = ResourceManager(rm_settings)
     assert len(rm.regions.regions) == 1, "Invalid number of regions returned"
