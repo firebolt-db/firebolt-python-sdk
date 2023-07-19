773567db0d9d3d6e2099f4971c4e15c0e6167616
RotemFB <rotem.bitner@firebolt.io>
2023-07-19 14:40:05 +0300

    pre commit modifications

--- tests/integration/resource_manager/test_region.py
+++ tests/integration/resource_manager/test_region.py
@@ -1,5 +1,6 @@
 from firebolt.service.manager import ResourceManager
 
+
 def test_get_region():
     rm = ResourceManager()
     assert len(rm.regions.regions) == 1, "Invalid number of regions returned"
