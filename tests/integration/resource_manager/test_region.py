67fe548822d5fe26ffa19ea62c523bd002e80502
RotemFB <rotem.bitner@firebolt.io>
2023-07-19 14:34:09 +0300

    added integration test for region

---
+++ tests/integration/resource_manager/test_region.py
@@ -0,0 +1,5 @@
+from firebolt.service.manager import ResourceManager
+
+def test_get_region():
+    rm = ResourceManager()
+    assert len(rm.regions.regions) == 1, "Invalid number of regions returned"
