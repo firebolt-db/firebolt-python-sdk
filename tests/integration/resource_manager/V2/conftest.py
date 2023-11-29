f810929f3525b9b4b0a97e48bc5d42efef17d214
ptiurin <petro.tiurin@firebolt.io>
2023-11-29 11:30:32 +0000

    fix fixture name

--- tests/integration/resource_manager/V2/conftest.py
+++ tests/integration/resource_manager/V2/conftest.py
@@ -2,15 +2,15 @@ from pytest import fixture
 
 
 @fixture
-def multi_param_engine_name(database: str) -> str:
-    return f"{database}_multi_param"
+def multi_param_engine_name(database_name: str) -> str:
+    return f"{database_name}_multi_param"
 
 
 @fixture
-def single_param_engine_name(database: str) -> str:
-    return f"{database}_single_param"
+def single_param_engine_name(database_name: str) -> str:
+    return f"{database_name}_single_param"
 
 
 @fixture
-def start_stop_engine_name(database: str) -> str:
-    return f"{database}_start_stop"
+def start_stop_engine_name(database_name: str) -> str:
+    return f"{database_name}_start_stop"
