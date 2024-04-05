21547c4afeef83f46bca047cad94adebd4f22b8e
ptiurin <petro.tiurin@firebolt.io>
2024-04-05 11:55:38 +0100

    fix: SQL syntax for alter engine in RM

--- tests/integration/dbapi/sync/V2/conftest.py
+++ tests/integration/dbapi/sync/V2/conftest.py
@@ -124,7 +124,7 @@ def service_account_no_user(
     with connection_system_engine_no_db.cursor() as cursor:
         cursor.execute(
             f'CREATE SERVICE ACCOUNT "{sa_account_name}" '
-            'WITH DESCRIPTION = "Ecosytem test with no user"'
+            "WITH DESCRIPTION = 'Ecosytem test with no user'"
         )
         cursor.execute(f"CALL fb_GENERATESERVICEACCOUNTKEY('{sa_account_name}')")
         # service_account_name, service_account_id, secret
