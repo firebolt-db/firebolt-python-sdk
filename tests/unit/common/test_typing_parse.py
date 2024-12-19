5139c732a9c3a8c3a55e34482a544391f676140a
Stepan Burlakov <stepan.burlakov@firebolt.io>
2024-12-19 14:28:05 +0200

    add struct type with spaces test

--- tests/unit/common/test_typing_parse.py
+++ tests/unit/common/test_typing_parse.py
@@ -35,6 +35,13 @@ def test_parse_type(types_map: Dict[str, type]) -> None:
     ), "Invalid type parsing error message"
 
 
+def test_parse_struct_type_with_spaces() -> None:
+    parsed = parse_type("struct(`a b` int, s struct(`c d` text))")
+    assert parsed == STRUCT(
+        {"a b": int, "s": STRUCT({"c d": str})}
+    ), f"Error parsing struct type with spaces"
+
+
 @mark.parametrize(
     "value,expected,error",
     [
