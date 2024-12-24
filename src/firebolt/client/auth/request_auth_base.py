427b94e13a06d7dd9da20054b61f756592dc70f6
ptiurin <petro.tiurin@firebolt.io>
2024-12-24 12:07:56 +0000

    fix errors

--- src/firebolt/client/auth/request_auth_base.py
+++ src/firebolt/client/auth/request_auth_base.py
@@ -1,11 +1,11 @@
 from time import time
 from typing import Generator
 
-from httpx import Request, Response
+from httpx import Request, Response, codes
 
 from firebolt.client.auth.base import Auth
 from firebolt.client.constants import _REQUEST_ERRORS
-from firebolt.utils.exception import AuthenticationError
+from firebolt.utils.exception import AuthenticationError, AuthorizationError
 from firebolt.utils.usage_tracker import get_user_agent_header
 
 
@@ -54,4 +54,6 @@ class _RequestBasedAuth(Auth):
             self._expires = int(time()) + int(parsed["expires_in"])
 
         except _REQUEST_ERRORS as e:
+            if e.response.status_code == codes.UNAUTHORIZED:
+                raise AuthorizationError()
             raise AuthenticationError(repr(e)) from e
