e882e71ee259ad24dc8f843faae83a9b07042967
Stepan Burlakov <stepan.burlakov@firebolt.io>
2025-01-09 13:58:12 +0200

    add comment on import

--- tests/integration/dbapi/async/V2/test_timeout.py
+++ tests/integration/dbapi/async/V2/test_timeout.py
@@ -3,6 +3,7 @@ from pytest import raises
 from firebolt.async_db import Connection
 from firebolt.utils.exception import QueryTimeoutError
 
+# Cannot have absolute path here since interpreter treats word async as keyword and not as a package name
 from .test_queries_async import LONG_SELECT
 
 
