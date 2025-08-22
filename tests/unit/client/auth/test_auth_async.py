94f01bd2160db396f0b71f782544f7c930346bd2
ptiurin <petro.tiurin@firebolt.io>
2025-08-22 18:01:16 +0100

    better java match

--- tests/unit/client/auth/test_auth_async.py
+++ tests/unit/client/auth/test_auth_async.py
@@ -88,7 +88,7 @@ async def test_auth_adds_header(access_token: str) -> None:
 
 
 @mark.nofakefs
-async def test_auth_token_storage(
+async def test_auth_token_caching(
     httpx_mock: HTTPXMock,
     client_id: str,
     client_secret: str,
